9b6497849212326cff9029ea69d92c2e571c218d
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-20 04:37:27 -0300

    "Practice Bootcamp"

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        // peso en kg / altura en metros al cuadrado
+        double peso = 82;
+        double altura = 1.82;
+        double indiceMasaCorporal = peso/(altura * altura);
+        System.out.println("Tu indice de masa corporal es: " + indiceMasaCorporal);
+    }
+}
