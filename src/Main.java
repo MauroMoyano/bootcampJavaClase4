a57c05382a04037c8ef6530795a27bc6a1862bec
MauroMoyano <mauromoyanodev@gmail.com>
2023-06-20 05:43:55 -0300

    api whatsApp para enviar msj

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,9 @@ public class Main {
         double altura = 1.82;
         double indiceMasaCorporal = peso/(altura * altura);
         System.out.println("Tu indice de masa corporal es: " + indiceMasaCorporal);
+
+        String whatsApp = "https://api.whatsapp.com/send?phone=";
+        System.out.println(whatsApp + "3855953");
+
     }
 }
